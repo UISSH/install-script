f960b0f490132be249dfa61da53150ff68ded0aa
z <zmaplex@gmail.com>
2022-08-04 13:38:27 +0800

    update

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ apt update -y && apt upgrade -y
 apt install python3 python3-pip curl git wget  curl wget git unzip dnsutils python3-venv python3-dev -y
 apt-get install libffi-dev -y
 pip3 install --upgrade pip
-pip3 install rich
+pip3 install rich wheel
 
 cd /tmp && git clone https://github.com/UISSH/install-script.git
