e69e896ad3d9882d320602c15b0aa0c2c10fbd4e
zmaplex <zmaplex@gmail.com>
2022-09-24 21:29:57 +0800

    Update

--- test_install.sh
+++ test_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-python3 main.py --login_email=root@root.com --db_root_password=root --login_password=root --test=true
+cd /tmp/install-script && python3 main.py --login_email=root@root.com --db_root_password=root --login_password=root --test=true
 
 if (/usr/local/uissh/backend/venv/bin/python3 /usr/local/uissh/backend/manage.py check); then
   echo 'ok'
