a044de77239f79c608204aed79ca3c1609b8e834
Zmaplex <zmaplex@gmail.com>
2023-03-29 17:24:08 +0800

    Updated: open 22,80,443 ports

--- install.sh
+++ install.sh
@@ -4,5 +4,8 @@ apt-get install libffi-dev default-libmysqlclient-dev build-essential -y
 pip3 install --upgrade pip
 pip3 install rich wheel
 
+# If ufw is already running, then open 22, 80, 443 ports
+ufw status | grep active | grep -q inactive || ufw allow 22/tcp && ufw allow 80/tcp && ufw allow 443/tcp
+
 cd /tmp && git clone https://github.com/UISSH/install-script.git
 
