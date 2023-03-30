fc02bad137edd03a45f39d53aa22287d126f4351
Zmaplex <zmaplex@gmail.com>
2023-03-30 20:46:26 +0800

    Updated: open 8080 port

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ pip3 install --upgrade pip
 pip3 install rich wheel
 
 # If ufw is already running, then open 22, 80, 443 ports
-ufw status | grep active | grep -q inactive || ufw allow 22/tcp && ufw allow 80/tcp && ufw allow 443/tcp
+ufw status | grep active | grep -q inactive || ufw allow 22/tcp && ufw allow 80/tcp && ufw allow 443/tcp  && ufw allow 8080/tcp
 
 cd /tmp && git clone https://github.com/UISSH/install-script.git
 
