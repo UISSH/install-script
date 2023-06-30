08291774cc7bca6b88b9603c378ca3883c06e7f8
zmaplex <zmaplex@gmail.com>
2023-06-30 13:11:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ logging.basicConfig(
 
 MIRROR_URL = "https://mirror-cloudflare.uissh.com/"
 CI_FLAG = False
-BACKEND_VERSION = "v0.2.6"
-FRONTEND_VERSION = "v0.2.6"
+BACKEND_VERSION = "v0.2.10"
+FRONTEND_VERSION = "v0.2.8"
 
 
 FRONTEND_URL = f"{MIRROR_URL}https://github.com/UISSH/react-frontend/releases/download/{FRONTEND_VERSION}/django_spa.zip"
